b9b992f6df364851a7b209eda68e66d38109fb5f
rabin-nyaundi <nyaundirabin@gmail.com>
2020-12-27 00:48:57 +0300

    adding items

--- tomhealth/models.py
+++ tomhealth/models.py
@@ -10,11 +10,20 @@ class Category(models.Model):
         return self.name
 
 
+class Benefits(models.Model):
+    name = models.CharField(max_length=255)
+    product_benefits = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     product_image = models.ImageField(upload_to='product_images/' ,null=True, blank=True)
     description = models.TextField(blank=True)
+    benefits = models.ForeignKey(Benefits, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -26,4 +35,4 @@ class Product(models.Model):
         except:
             url = ''
         return url
-
+        
